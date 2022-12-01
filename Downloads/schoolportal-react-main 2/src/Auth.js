e9cc1b06d09ddd895b330b18f7221bb3275b0462
RONAK PRAJAPATI <ronakprajapati107@gmail.com>
2022-12-01 17:13:55 +0530

    Delete Downloads/schoolportal-react-main 2 directory

--- Downloads/schoolportal-react-main 2/src/Auth.js
+++
@@ -1,9 +0,0 @@
-const token = localStorage.getItem("studyhox");
-
-const auth = {
-  headers: {
-    Authorization: `Bearer ${token}`,
-    "Access-Control-Allow-Origin": "*",
-  },
-};
-export default auth;
--- Downloads/schoolportal-react-main 2/src/Baseurl.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Baseurl = () => {
-  return "http://ec2-13-232-130-7.ap-south-1.compute.amazonaws.com/api/";
-};
-
-export default Baseurl;
